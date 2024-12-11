75c5ab7412feb6909eb6203c437b3382f42b39e7
flashcode <laughingliangcl@gmail.com>
2024-12-11 09:34:46 +0800

    feat: use openai sdk

--- llm.py
+++ llm.py
@@ -1,5 +1,7 @@
 import requests
 from dotenv import load_dotenv
+from openai import OpenAI
+
 import os
 
 
@@ -35,5 +37,23 @@ def main():
     if response.status_code != 200:
         print('Error:', response_json.get('error', 'Unknown error occurred'))
 
+def use_openai_sdk():
+    api_key = os.getenv("HEURIST_API_KEY")
+    client = OpenAI(base_url='https://llm-gateway.heurist.xyz', api_key=api_key)
+    model = 'meta-llama/llama-3.3-70b-instruct'
+    print('Sending request to LLM API...')
+    result = client.chat.completions.create(
+        model=model,
+        messages=[{
+            "role": "user",
+            "content": "hello"
+        }],
+        stream=False,
+        temperature=0.5,
+    )
+    print('result: ', result.choices[0].message.content)
+
+
 if __name__ == "__main__":
-    main()
+    use_openai_sdk()
+    # main()
